33822bf450423e7aa4a4e532b54e63bacf6ad0e0
Sai Durga Praveena Rimmalapudi <rimmalapudipraveena@gmail.com>
2025-12-24 19:44:00 +0530

    Fix segmentation fault: Move migrations to startup and make TensorFlow imports lazy

--- Django/predict/__init__.py
+++ Django/predict/__init__.py
@@ -1,3 +1,24 @@
-from .prediction import get_live_data, get_live_prediction, load_model_and_scaler
+# Lazy imports to avoid loading TensorFlow during Django startup/migrations
+# Import these functions only when needed, not at module level
 
 __all__ = ['get_live_data', 'get_live_prediction', 'load_model_and_scaler']
+
+def _lazy_import():
+    """Lazy import to avoid TensorFlow loading during Django initialization"""
+    from .prediction import get_live_data, get_live_prediction, load_model_and_scaler
+    return get_live_data, get_live_prediction, load_model_and_scaler
+
+def get_live_data(*args, **kwargs):
+    """Lazy wrapper for get_live_data"""
+    from .prediction import get_live_data
+    return get_live_data(*args, **kwargs)
+
+def get_live_prediction(*args, **kwargs):
+    """Lazy wrapper for get_live_prediction"""
+    from .prediction import get_live_prediction
+    return get_live_prediction(*args, **kwargs)
+
+def load_model_and_scaler(*args, **kwargs):
+    """Lazy wrapper for load_model_and_scaler"""
+    from .prediction import load_model_and_scaler
+    return load_model_and_scaler(*args, **kwargs)
