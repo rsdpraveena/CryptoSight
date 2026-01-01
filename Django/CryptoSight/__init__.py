5900bc7159075c2b4174ed3c2ce44f6e2436eb9f
Sai Durga Praveena Rimmalapudi <rimmalapudipraveena@gmail.com>
2026-01-01 20:23:49 +0530

    Fix: Handle missing Celery in __init__.py

--- Django/CryptoSight/__init__.py
+++ Django/CryptoSight/__init__.py
@@ -1,3 +1,13 @@
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
+try:
+    from .celery import app as celery_app
+    __all__ = ('celery_app',)
+except (ImportError, ModuleNotFoundError):
+    # Celery is not installed, create a dummy app
+    class DummyCelery:
+        def task(self, *args, **kwargs):
+            if len(args) == 1 and callable(args[0]):
+                return args[0]
+            return lambda f: f
+    
+    celery_app = DummyCelery()
+    __all__ = ('celery_app',)
